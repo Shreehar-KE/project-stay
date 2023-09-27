c7a18232559a69a8d37e0649fe5748b3740f5770
Shreehar-KE <shreehar.elancheran@gmail.com>
2023-09-28 03:42:13 +0530

    Added the major routes

--- stay-frontend/src/main.jsx
+++ stay-frontend/src/main.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Home from './pages/home/Home'
+import List from './pages/list/List'
+import Hotel from './pages/hotel/Hotel'
+import Checkout from './pages/checkout/Checkout'
+import Dashboard from './pages/dashboard/Dashboard'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/hotels",
+    element: <List/>,
+  },
+  {
+    path: "/hotels/:id",
+    element: <Hotel/>,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout/>,
+  },
+  {
+    path: "/user/:id",
+    element: <Dashboard/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
